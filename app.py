035b1cde28dabf96588c8941b6a7bdbb4740dd56
Gregory White <gcwhitemail@gmail.com>
2024-01-24 14:22:30 +0000

    Implementing sessions and rerolls
    Implementing sessions so that character attributes can remain on screen and single attributes can be rerolled independently. Restructured the character generation to work with a dictionary.

--- app.py
+++ app.py
@@ -1,26 +1,32 @@
 import random
-from flask import Flask, render_template
+from flask import Flask, render_template, request, session
 
 app = Flask(__name__)
+app.secret_key = 'your_secret_key'
 
-
-## Name Generation
-def roll_name():
-    # Generate a random uppercase letter for first and last names
+## Roll First Name
+def roll_first_name():
+    # Generate a random uppercase letter for first name
     ascii = random.randint(ord('A'), ord('Z'))
-    first = chr(ascii)
-
-    ascii = random.randint(ord('A'), ord('Z'))
-    last = chr(ascii)
+    first_name = chr(ascii)
 
     # Print name results:
-    print("First Name:", first)
-    print("Last Name: ", last)
-
-    return first, last
+    print("First Name:", first_name)
+    return first_name
 
 
-## Alignment Chart
+## Roll Last Name
+def roll_last_name():
+    # Generate a random uppercase letter for last name
+    ascii = random.randint(ord('A'), ord('Z'))
+    last_name = chr(ascii)
+
+    # Print name results:
+    print("Last Name: ", last_name)
+    return last_name
+
+
+## Roll Alignment
 def roll_alignment():
     # Create a variable for lawful vs chaotic
     method = random.randint(0, 2)
@@ -49,42 +55,59 @@ def roll_alignment():
     # Print alignment
     alignment = method + " " + morals
     print("Alignment: ", alignment)
-
     return alignment
 
 
-## Race Selection
+## Roll Race
 def roll_race():
     # Create a list of races
     races = ['Dragonborn', 
-             'Dwarf', 
-             'Elf', 
-             'Gnome', 
-             'Half-Elf', 
-             'Halfling',
-             'Half-Orc',
-             'Human',
-             'Tiefling']
-    
+            'Dwarf', 
+            'Elf', 
+            'Gnome', 
+            'Half-Elf', 
+            'Halfling',
+            'Half-Orc',
+            'Human',
+            'Tiefling']
     # Select a race by rolling a number between 0 and the length of the races list
-    race = races[random.randint(0, len(races))]
-
+    race = random.choice(races)
     # Print race
     print("Race:      ", race)
-
     return race
+
+
+## Roll Full Character
+def roll_character():
+    return {
+        'first_name': roll_first_name(),
+        'last_name': roll_last_name(),
+        'alignment': roll_alignment(),
+        'race': roll_race()
+    }
 
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
     """Generate Character"""
 
-    # Roll character attributes
-    first, last = roll_name()
-    alignment = roll_alignment()
-    race = roll_race()
+    # If a character has not yet been generated, make one
+    if 'character' not in session:
+        session['character'] = roll_character()
 
-    return render_template("index.html", first=first, last=last, alignment=alignment, race=race)
+    character = session['character']
+
+    # If a button is clicked...
+    if request.method == 'POST':
+        # ...check which button was clicked
+        if request.form.get('roll_character'):
+            session['character'] = roll_character()
+            character = session['character']
+            
+        elif request.form.get('roll_race'):
+            character['race'] = roll_race()
+            
+    return render_template("index.html", character=character)
 
 
 if __name__ == '__main__':
